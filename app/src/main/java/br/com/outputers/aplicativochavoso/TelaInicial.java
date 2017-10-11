31e600297a2f048cbb1de3614e8882d578013969
Youssef <emaildeyoussefmuhamad@gmail.com>
2017-10-11 17:19:14 -0300

    Commit inical - Diretório do Projeto

---
+++ app/src/main/java/br/com/outputers/aplicativochavoso/TelaInicial.java
@@ -0,0 +1,18 @@
+package br.com.outputers.aplicativochavoso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaInicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+
+    private void TestUpload(){
+        int i = 1+1;
+    }
+
+}
