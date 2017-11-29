402843f85455be5d77363a9e74185bd3e130c5ff
Youssef <emaildeyoussefmuhamad@gmail.com>
2017-11-28 22:26:36 -0200

    implementação do splash

--- app/src/main/java/br/com/outputers/aplicativochavoso/Inicial.java
+++ app/src/main/java/br/com/outputers/aplicativochavoso/Inicial.java
@@ -3,40 +3,61 @@ package br.com.outputers.aplicativochavoso;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.drawable.AnimatedStateListDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class Inicial extends AppCompatActivity {
 
-    ImageButton btnTelaInicial;
+    private ImageView imgViewLogo;
 
     SharedPreferences CadastroPreferences;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicial2);
 
+        imgViewLogo = findViewById(R.id.logo_chavoso);
 
-        CadastroPreferences = getSharedPreferences("cadastro",Context.MODE_PRIVATE);
-        Boolean cadastrou = CadastroPreferences.getBoolean("cadastrou", false);
-        Log.e("CADASTROU", cadastrou.toString());
+        //splash
+        Animation myanim = AnimationUtils.loadAnimation(this,R.anim.mytransition);
+        imgViewLogo.startAnimation(myanim);
 
-        if(cadastrou) {
-            intentEntrar2Vez(); // Caso o usuário já seja cadastrado
-        }
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
 
-        btnTelaInicial = findViewById(R.id.logo);
-        btnTelaInicial.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                intentEntrar1Vez();
+                    CadastroPreferences = getSharedPreferences("cadastro",Context.MODE_PRIVATE);
+                    Boolean cadastrou = CadastroPreferences.getBoolean("cadastrou", false);
+                    Log.e("CADASTROU", cadastrou.toString());
+
+                    if(cadastrou) {
+                        intentEntrar2Vez(); // Caso o usuário já seja cadastrado
+                    }else{
+                        intentEntrar1Vez();
+                    }
+                }
             }
-        }) ;
+        };
+
+
+        timer.start();
+
+
     }
 
     private void intentEntrar2Vez() {
